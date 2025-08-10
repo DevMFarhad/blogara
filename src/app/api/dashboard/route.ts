3f03289cb4ec0c3b11ac0adceac0efda6ba1945c
Mohammad <mfarhad.dev@gmail.com>
2025-08-10 18:53:00 +0600

    implemented dashboard stats apis

---
+++ src/app/api/dashboard/route.ts
@@ -0,0 +1,71 @@
+import prisma from "@/lib/prisma";
+import catchAsync from "@/lib/utils/catchAsync";
+import apiAuthValidator from "@/lib/utils/apiAuthValidator";
+import sendResponse from "@/lib/utils/sendResponse";
+
+export const GET = catchAsync(async (req) => {
+    await apiAuthValidator(["admin", "author"])(req);
+    const user = req.user!;
+
+    if (user.role === "author") {
+        const [totalPostCount, publishedPostsCount, unpublishedPostCount] = await Promise.all([
+            prisma.post.count({ where: { authorId: user.id } }),
+            prisma.post.count({ where: { authorId: user.id, isPublished: true } }),
+            prisma.post.count({ where: { authorId: user.id, isPublished: false } }),
+        ]);
+
+        return sendResponse({
+            success: true,
+            data: {
+                totalPostCount,
+                publishedPostsCount,
+                unpublishedPostCount
+            },
+        });
+    }
+
+    else {
+        const [
+            totalPosts,
+            publishedPosts,
+            unpublishedPosts,
+            totalUsers,
+            activeUsers,
+            inactiveUsers,
+            totalSubscribers,
+            activeSubscribers,
+            inactiveSubscribers,
+        ] = await Promise.all([
+            prisma.post.count(),
+            prisma.post.count({ where: { isPublished: true } }),
+            prisma.post.count({ where: { isPublished: false } }),
+            prisma.user.count(),
+            prisma.user.count({ where: { isActive: true } }),
+            prisma.user.count({ where: { isActive: false } }),
+            prisma.subscriber.count(),
+            prisma.subscriber.count({ where: { isActive: true } }),
+            prisma.subscriber.count({ where: { isActive: false } }),
+        ]);
+
+        return sendResponse({
+            success: true,
+            data: {
+                posts: {
+                    total: totalPosts,
+                    published: publishedPosts,
+                    unpublished: unpublishedPosts,
+                },
+                users: {
+                    total: totalUsers,
+                    active: activeUsers,
+                    inactive: inactiveUsers,
+                },
+                subscribers: {
+                    total: totalSubscribers,
+                    active: activeSubscribers,
+                    inactive: inactiveSubscribers,
+                },
+            },
+        });
+    }
+});
--- src/lib/utils/catchAsync.ts
+++ src/lib/utils/catchAsync.ts
@@ -2,21 +2,21 @@ import { NextRequest, NextResponse } from 'next/server';
 import ApiError from './ApiError';
 
 export default function catchAsync<
-  Params extends Record<string, string> = Record<string, string>,
+  Params extends Record<string, string> = Record<string, string>
 >(
   handler: (
     req: NextRequest,
-    context: { params: Params },
-  ) => Promise<NextResponse>,
+    context?: { params: Params }
+  ) => Promise<NextResponse>
 ) {
-  return async (req: NextRequest, context: { params: Params }) => {
+  return async (req: NextRequest, context?: { params: Params }) => {
     try {
       return await handler(req, context);
     } catch (error: any) {
       const status = error instanceof ApiError ? error.statusCode : 500;
       return NextResponse.json(
         { success: false, message: error.message || 'Internal Server Error' },
-        { status },
+        { status }
       );
     }
   };
